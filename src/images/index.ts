bca9b2be34e9b59351e6e1f4246b058c730aa634
ekadaegile <egileoniso.ekada@gmail.com>
2024-09-18 00:20:14 +0100

    update on images

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,9 +1,9 @@
 import hamsterExchange from "./hamster-exchange.png";
 import binanceLogo from "./toncoin-ton-logo.png";
 import dollarCoin from "./main-character.png";
-import dailyReward from "./daily-reward.png";
-import dailyCipher from "./daily-cipher.png";
-import dailyCombo from "./daily-combo.png";
+import dailyReward from "./reward.png";
+import dailyCipher from "./ciper.png";
+import dailyCombo from "./combo.png";
 import mainCharacter from "./main.png";
 import hamsterCoin from "./main.png";
 
